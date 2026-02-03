3ccafbcb68d41091b1e873959f6f3988abdc811a
Aida Carabot <aidacarabot@MacBook-Pro-de-Aida-2.local>
2026-02-03 17:31:45 -0500

    Login and Register Done

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,13 +1,19 @@
 import './RegisterForm.css';
+import { LockKeyhole, UserRound, Calendar, Eye, EyeClosed, Mail } from 'lucide-react';
 import { useForm } from 'react-hook-form';
 import { fetchData } from '../../utils/api/fetchData';
 import { useState } from 'react';
 import Logo from '../Logo/Logo';
+import Loader from '../Loader/Loader';
+import { SuccessMessage, ErrorMessage } from '../Messages/Messages';
 
 const RegisterForm = ({ onToggleForm }) => {
   const { register, handleSubmit, formState: { errors }, getValues } = useForm();
   const [showPassword, setShowPassword] = useState(false);
   const [showRepeatPassword, setShowRepeatPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   // Manejo de la validación de contraseñas
   const validatePasswordMatch = (value) => {
@@ -18,24 +24,37 @@ const RegisterForm = ({ onToggleForm }) => {
   };
 
   const onSubmit = async (formData) => {
+    setIsLoading(true);
+    setErrorMessage('');
+    setSuccessMessage('');
+    
     try {
-      const response = await fetchData(
+      await fetchData(
         '/users/register',
         'POST',
         formData
       );
-      console.log('Registration successful:', response);
-      alert('Registration successful. Please log in.');
-      onToggleForm();
+      setSuccessMessage('Registration successful. Please log in.');
+      setTimeout(() => {
+        onToggleForm();
+      }, 2000);
     } catch (error) {
       const errorMsg = error?.response?.error || 'There was an error during registration. Please try again.';
-      alert(errorMsg);
+      setErrorMessage(errorMsg);
       console.error('Error during registration:', error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
     <div className="register-form-container">
+      {successMessage && <SuccessMessage text={successMessage} />}
+      {errorMessage && <ErrorMessage text={errorMessage} />}
       <div className="register-form-header">
         <Logo />
       </div>
@@ -48,9 +67,7 @@ const RegisterForm = ({ onToggleForm }) => {
           <div className="form-group">
             <div className="input-wrapper">
               <span className="input-icon">
-                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M10 10C11.6569 10 13 8.65685 13 7C13 5.34315 11.6569 4 10 4C8.34315 4 7 5.34315 7 7C7 8.65685 8.34315 10 10 10Z M10 10C7.23858 10 5 12.2386 5 15V16.5M10 10C12.7614 10 15 12.2386 15 15V16.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
+                <UserRound size={20} />
               </span>
               <input 
                 {...register("name", { required: "Name is required" })} 
@@ -65,9 +82,7 @@ const RegisterForm = ({ onToggleForm }) => {
           <div className="form-group">
             <div className="input-wrapper">
               <span className="input-icon">
-                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M5.83333 4.16667H14.1667C15.0871 4.16667 15.8333 4.91286 15.8333 5.83333V14.1667C15.8333 15.0871 15.0871 15.8333 14.1667 15.8333H5.83333C4.91286 15.8333 4.16667 15.0871 4.16667 14.1667V5.83333C4.16667 4.91286 4.91286 4.16667 5.83333 4.16667Z M13.3333 2.5V5.83333 M6.66667 2.5V5.83333 M4.16667 9.16667H15.8333" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
+                <Calendar size={20} />
               </span>
               <input 
                 {...register("birthDate", { required: "Birth Date is required" })} 
@@ -84,9 +99,7 @@ const RegisterForm = ({ onToggleForm }) => {
           <div className="form-group">
             <div className="input-wrapper">
               <span className="input-icon">
-                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M2.5 6.66669L9.0755 11.0504C9.63533 11.4236 10.3647 11.4236 10.9245 11.0504L17.5 6.66669M4.16667 15H15.8333C16.7538 15 17.5 14.2538 17.5 13.3334V6.66669C17.5 5.74621 16.7538 5.00002 15.8333 5.00002H4.16667C3.24619 5.00002 2.5 5.74621 2.5 6.66669V13.3334C2.5 14.2538 3.24619 15 4.16667 15Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
+                <Mail size={20} />
               </span>
               <input 
                 {...register("email", { required: "Email is required" })} 
@@ -101,9 +114,7 @@ const RegisterForm = ({ onToggleForm }) => {
           <div className="form-group">
             <div className="input-wrapper">
               <span className="input-icon">
-                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M5.83333 9.16669V6.66669C5.83333 4.36545 7.69881 2.50002 10 2.50002C12.3012 2.50002 14.1667 4.36545 14.1667 6.66669V9.16669M10 12.0834V13.75M6.5 17.5H13.5C14.4205 17.5 15.1667 16.7538 15.1667 15.8334V10.8334C15.1667 9.91288 14.4205 9.16669 13.5 9.16669H6.5C5.57953 9.16669 4.83333 9.91288 4.83333 10.8334V15.8334C4.83333 16.7538 5.57953 17.5 6.5 17.5Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
+                <LockKeyhole size={20} />
               </span>
               <input 
                 {...register("password", { required: "Password is required" })} 
@@ -116,13 +127,7 @@ const RegisterForm = ({ onToggleForm }) => {
                 className="toggle-password"
                 onClick={() => setShowPassword(!showPassword)}
               >
-                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  {showPassword ? (
-                    <path d="M2.5 10C2.5 10 5 4.16669 10 4.16669C15 4.16669 17.5 10 17.5 10C17.5 10 15 15.8334 10 15.8334C5 15.8334 2.5 10 2.5 10Z M10 12.5C11.3807 12.5 12.5 11.3807 12.5 10C12.5 8.61931 11.3807 7.50002 10 7.50002C8.61929 7.50002 7.5 8.61931 7.5 10C7.5 11.3807 8.61929 12.5 10 12.5Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                  ) : (
-                    <path d="M3.53553 3.53555L16.4645 16.4645M8.17157 8.17159C7.42143 8.92173 7.42143 10.1373 8.17157 10.8874C8.92172 11.6376 10.1373 11.6376 10.8874 10.8874M8.17157 8.17159L10.8874 10.8874M8.17157 8.17159L6.46967 6.46968M10.8874 10.8874L12.7728 12.7728M6.46967 6.46968C7.71627 5.65704 9.19977 5.16669 10.8284 5.16669C15.5 5.16669 17.5 10 17.5 10C17.1478 10.7069 16.5993 11.5183 15.8891 12.2786M6.46967 6.46968L2.5 2.50002M12.7728 12.7728C11.7363 13.4821 10.4824 13.8334 9.17157 13.8334C4.5 13.8334 2.5 9.00002 2.5 9.00002C2.93508 8.13308 3.61824 7.19302 4.53553 6.34847L12.7728 12.7728Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                  )}
-                </svg>
+                {showPassword ? <Eye size={20} /> : <EyeClosed size={20} />}
               </button>
             </div>
             {errors.password && <p className="error-message">{errors.password.message}</p>}
@@ -131,9 +136,7 @@ const RegisterForm = ({ onToggleForm }) => {
           <div className="form-group">
             <div className="input-wrapper">
               <span className="input-icon">
-                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M5.83333 9.16669V6.66669C5.83333 4.36545 7.69881 2.50002 10 2.50002C12.3012 2.50002 14.1667 4.36545 14.1667 6.66669V9.16669M10 12.0834V13.75M6.5 17.5H13.5C14.4205 17.5 15.1667 16.7538 15.1667 15.8334V10.8334C15.1667 9.91288 14.4205 9.16669 13.5 9.16669H6.5C5.57953 9.16669 4.83333 9.91288 4.83333 10.8334V15.8334C4.83333 16.7538 5.57953 17.5 6.5 17.5Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
+                <LockKeyhole size={20} />
               </span>
               <input 
                 {...register("repeatPassword", { 
@@ -149,13 +152,7 @@ const RegisterForm = ({ onToggleForm }) => {
                 className="toggle-password"
                 onClick={() => setShowRepeatPassword(!showRepeatPassword)}
               >
-                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  {showRepeatPassword ? (
-                    <path d="M2.5 10C2.5 10 5 4.16669 10 4.16669C15 4.16669 17.5 10 17.5 10C17.5 10 15 15.8334 10 15.8334C5 15.8334 2.5 10 2.5 10Z M10 12.5C11.3807 12.5 12.5 11.3807 12.5 10C12.5 8.61931 11.3807 7.50002 10 7.50002C8.61929 7.50002 7.5 8.61931 7.5 10C7.5 11.3807 8.61929 12.5 10 12.5Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                  ) : (
-                    <path d="M3.53553 3.53555L16.4645 16.4645M8.17157 8.17159C7.42143 8.92173 7.42143 10.1373 8.17157 10.8874C8.92172 11.6376 10.1373 11.6376 10.8874 10.8874M8.17157 8.17159L10.8874 10.8874M8.17157 8.17159L6.46967 6.46968M10.8874 10.8874L12.7728 12.7728M6.46967 6.46968C7.71627 5.65704 9.19977 5.16669 10.8284 5.16669C15.5 5.16669 17.5 10 17.5 10C17.1478 10.7069 16.5993 11.5183 15.8891 12.2786M6.46967 6.46968L2.5 2.50002M12.7728 12.7728C11.7363 13.4821 10.4824 13.8334 9.17157 13.8334C4.5 13.8334 2.5 9.00002 2.5 9.00002C2.93508 8.13308 3.61824 7.19302 4.53553 6.34847L12.7728 12.7728Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                  )}
-                </svg>
+                {showRepeatPassword ? <Eye size={20} /> : <EyeClosed size={20} />}
               </button>
             </div>
             {errors.repeatPassword && <p className="error-message">{errors.repeatPassword.message}</p>}
