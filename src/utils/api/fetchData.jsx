91298b9e9aeebfb1478a7c977965d453c24845b3
Aida Carabot <aidacarabot@gmail.com>
2026-04-20 21:31:15 -0400

    backend attached

--- src/utils/api/fetchData.jsx
+++ src/utils/api/fetchData.jsx
@@ -1,5 +1,5 @@
 
-const BASE_URL = "http://localhost:3000/api/v1";
+const BASE_URL = "https://backend-savings-app.vercel.app/api/v1";
 
 export const fetchData = async (endpoint, method = "GET", data = null, headers = {}) => {
    try {
